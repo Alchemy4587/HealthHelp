663af4967176ae6d4bf5c054c124afcd6fc34fc1
unknown <kanjoel39@gmail.com>
2023-09-09 12:41:52 +0100

    HealthHelp

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        if(Auth::id())
+        {
+            if(Auth::user()->usertype=='0')
+            {
+                return view('admin.home');
+            }
+            else
+            {
+                return view('admin.home');
+            }
+        }
+
+        else
+        {
+            return redirect()->back();
+        }
+    }
+    public function index()
+    {
+        return view('user.home');
+    }
+}
